b33179c80e2efade75db093f4ea479ffb6602a90
Uyencute23 <91412961+Uyencute23@users.noreply.github.com>
2022-06-16 19:47:00 +0700

    hihi

--- resources/views/admin/layouts/sidenav.blade.php
+++ resources/views/admin/layouts/sidenav.blade.php
@@ -6,7 +6,7 @@
         <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
             target="_blank">
             <img src="{{ asset('admin/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+            <span class="ms-1 font-weight-bold">Chicken Cool</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
